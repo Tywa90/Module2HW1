9cbbe0215318028eb4fd33dbd960618e1eb4b215
Serhii Amelin <amserega90@gmail.com>
2023-03-28 01:14:57 +0300

    new project LoggerApp

---
+++ LoggerApp/LoggerApp.Code/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggerApp.Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
